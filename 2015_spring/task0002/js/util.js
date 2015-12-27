22fb671485f1942a20b1f67f2e7a70efbafe7cf8
codecly <maxinchun0215@163.com>
2015-12-27 20:13:45 +0800

    add functiuons in util.js

--- 2015_spring/task0002/js/util.js
+++ 2015_spring/task0002/js/util.js
@@ -74,7 +74,7 @@ console.log(tarObj.b.b1[0]);
 function uniqArray(arr) {
     var newArr = [];
     for (var i in arr) {
-        if(newArr.indexOf(arr[i]) == -1){
+        if (newArr.indexOf(arr[i]) == -1) {
             newArr.push(arr[i]);
         }
     }
@@ -87,6 +87,79 @@ var a = [1, 3, 5, 7, 5, 3];
 var b = uniqArray(a);
 console.log(b);
 
+// 实现一个简单的trim函数，用于去除一个字符串，头部和尾部的空白字符
+// 假定空白字符只有半角空格、Tab
+// 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
+function simpleTrim(str) {
+    var i;
+    var j;
+    for (i = 0; i < str.length; i++) { //从头开始遍历字符串
+        if (str[i] != " " && str[i] != "\t") {//当字符不为空的时候
+            break;
+        }
+    }
+
+    for (j = str.length - 1; j >= 0; j--) { //从字符串尾部遍历
+        if (str[j] != " " && str[j] != "\t") { //当字符不为空的时候
+            break;
+        }
+    }
+
+    return str.slice(i, j + 1);
+
+}
+
+// 对字符串头尾进行空格字符串的去除、包括全角半角空格、Tab等，返回一个字符串
+// 尝试使用一行简洁的正则表达式完成该题目
+function trim(str) {
+    if (str.length != -1) {
+        return str.replace(/^\s+|\s+$/g, '');
+    }
+}
+
+// 使用示例
+var str = ' hi!  ';
+str = trim(str);
+console.log(str);  //'hi!'
+
+// 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
+// 其中fn函数可以接受两个参数：item和index
+function each(arr, fn) {
+    for (var i in arr) {
+        fn(arr[i], i);
+    }
+}
+
+// 使用示例
+var arr = ['java', 'c', 'php', 'html'];
+function output(item, index) {
+    console.log(index + ':' + item);
+}
+
+each(arr, output); // 0:java, 1:c, 2:php, 3:html
+
+
+// 获取一个对象里面第一层元素的数量，返回一个整数
+function getObjectLength(obj) {
+    var length = 0;
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            length++;
+        }
+    }
+    return length;
+}
+
+// 使用示例
+var obj = {
+    a: 1,
+    b: 2,
+    c: {
+        c1: 3,
+        c2: 4
+    }
+}
+console.log(getObjectLength(obj));
 
 
 
