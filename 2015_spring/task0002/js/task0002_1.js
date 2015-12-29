b7dd7fe4a38cc1b15fc39c3396f7397ac99e9da8
codecly <maxinchun0215@163.com>
2015-12-29 15:03:38 +0800

    添加小练习1的html和js

---
+++ 2015_spring/task0002/js/task0002_1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by maxinchun on 2015/12/29.
+ */
+function addEvent(){
+    //alert(1);
+    console.info($("button"));
+    $.click($("button"), function () {
+        alert(1);
+    });
+}
+addEvent();
